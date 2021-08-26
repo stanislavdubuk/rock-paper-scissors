13d858cb3151b849e1402ad4d062116b76ea0f58
stanislavdubuk <stanislavdubuk@gmail.com>
2021-08-26 16:15:31 +0300

    build fix

--- src/store/scoreSlice.ts
+++ src/store/scoreSlice.ts
@@ -1,6 +1,4 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-
-// import { RootState } from './store';
+import { createSlice } from '@reduxjs/toolkit';
 
 interface GameState {
   score: number;
